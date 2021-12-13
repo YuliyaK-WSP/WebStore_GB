59d162e61c07ffa2911179e6819c787d07ba85b5
Yuliya <yvnovikova@mail.ru>
2021-12-13 14:21:53 +0300

    Добавление первого контроллера

--- WebStore/Program.cs
+++ WebStore/Program.cs
@@ -1,11 +1,21 @@
 var builder = WebApplication.CreateBuilder(args);
+var services = builder.Services;
+services.AddControllersWithViews();
 var app = builder.Build();
 
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+app.UseRouting();
 // Загрузка информации из файла конфигурации
 
-var configuration = app.Configuration;
-
-var greetings = configuration["CustomGreetings"];
-app.MapGet("/", () => greetings);
-
+//var configuration = app.Configuration;
+//var greetings = configuration["CustomGreetings"];
+//app.MapGet("/", () => app.Configuration["CustomGreetings"]);
+app.MapGet("/thow", () =>
+{
+    throw new ApplicationException("Ошибка в программе!");
+});
+app.MapDefaultControllerRoute();
 app.Run();
