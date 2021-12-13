59b96e9df4a37c1db9214921926a7f14bcd41462
Yuliya <yvnovikova@mail.ru>
2021-12-13 23:32:18 +0300

    Дополнен __Details

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -16,11 +16,11 @@ namespace WebStore.Controllers
         };
         private static readonly List<EmployeeView> __Details = new()
         {
-            new EmployeeView { Id = 1,  Description = "Хроший сотрудник один" },
-            new EmployeeView { Id = 2,  Description = "Хроший сотрудник два" },
-            new EmployeeView { Id = 3,  Description = "Хроший сотрудник три" },
-            new EmployeeView { Id = 4,  Description = "Хроший сотрудник четыре" },
-            new EmployeeView { Id = 5,  Description = "Хроший сотрудник пять" },
+            new EmployeeView { Id = 1, LastName = "Иванов", FirstName = "Иван", Patronymic = "Иванович", Age = 23, Profession = "Инженер - Разрабочик", Department = 56,  Description = "Хроший сотрудник один" },
+            new EmployeeView { Id = 2, LastName = "Петров", FirstName = "Петр", Patronymic = "Петрович", Age = 27, Profession = "Инженер - Разработчик", Department = 56,  Description = "Хроший сотрудник два" },
+            new EmployeeView { Id = 3, LastName = "Сидоров", FirstName = "Сидр", Patronymic = "Сидорович", Age = 18, Profession = "Аналитик", Department = 46,  Description = "Хроший сотрудник три" },
+            new EmployeeView { Id = 4, LastName = "Васильев", FirstName = "Василий", Patronymic = "Васильевич", Age = 46, Profession = "Системный Администратор", Department = 56,  Description = "Хроший сотрудник четыре" },
+            new EmployeeView { Id = 5, LastName = "Сергеев", FirstName = "Сергей", Patronymic = "Алексеевич", Age = 35, Profession = "Аналитик", Department = 46,  Description = "Хроший сотрудник пять" },
 
         };
         public IActionResult Index()
--- WebStore/Models/EmployeeView.cs
+++ WebStore/Models/EmployeeView.cs
@@ -9,7 +9,12 @@ namespace WebStore.Models
     public class EmployeeView
     {
         public int Id { get; set; }
-        
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string Patronymic { get; set; }
+        public int Age { get; set; }
+        public string Profession { get; set; }
+        public int Department { get; set; }
         public string Description { get; set; }
 
     }
