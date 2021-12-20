98e70b2543e1b1a75d0f85620ea3767061ce79a0
Yuliya <yvnovikova@mail.ru>
2021-12-20 21:24:21 +0300

    Реализовано удаление и создание сотрудников. Проставленны ссылки

--- WebStore/Controllers/EmployeesController.cs
+++ WebStore/Controllers/EmployeesController.cs
@@ -10,36 +10,39 @@ namespace WebStore.Controllers
     public class EmployeesController : Controller
     {
         
-        private readonly IEmployeesData __EmployeesData;
-
-        public EmployeesController(IEmployeesData EmployeesData)
+        private readonly IEmployeesData _EmployeesData;
+        private readonly ILogger<EmployeesController> _Logger;
+        public EmployeesController(IEmployeesData EmployeesData, ILogger<EmployeesController> Logger)
         {
-           
-            __EmployeesData = EmployeesData;
+
+            _EmployeesData = EmployeesData;
+            _Logger = Logger;
         }
         public IActionResult Index()
         {
-            return View(__EmployeesData.GetAll());
+            return View(_EmployeesData.GetAll());
         }
         public IActionResult Details(int Id)
         {
             //var employee = __Employees.FirstOrDefault(e => e.Id == Id);
-            var employee = __EmployeesData.GetById(Id);
+            var employee = _EmployeesData.GetById(Id);
             if (employee is null)
             {
                 return NotFound();
             }
             return View(employee);
         }
-        //public IActionResult Create() => View();
+        public IActionResult Create() => View("Edit",new EmployeeViewModel());
 
         public IActionResult Delete(int id)
         {
             if (id < 0)
                 return BadRequest();
-            var employee = __EmployeesData.GetById(id);
+
+            var employee = _EmployeesData.GetById(id);
             if (employee is null)
                 return NotFound();
+
             var model = new EmployeeViewModel
             {
                 Id = employee.Id,
@@ -55,17 +58,25 @@ namespace WebStore.Controllers
         [HttpPost]
         public IActionResult DeleteConfirmed(int id)
         {
-            if (__EmployeesData.Delete(id))
+            if (!_EmployeesData.Delete(id))
                 return NotFound();
+
+            _Logger.LogInformation("Сотрудник с id:{0} удалён", id);
+
             return RedirectToAction("Index");
         }
 
-        public IActionResult Edit(int id)
+        public IActionResult Edit(int? id)
         {
             //var employee = __Employees.FirstOrDefault(e => e.Id == id);
-            var employee = __EmployeesData.GetById(id);
+            if (id is null)
+                return View(new EmployeeViewModel());
+
+            //var employee = __Employees.FirstOrDefault(e => e.Id == id);
+            var employee = _EmployeesData.GetById((int)id);
             if (employee is null)
             {
+                _Logger.LogWarning("При редактировании сотрудника с id:{0} он не был найден", id);
                 return NotFound();
             }
             var model = new EmployeeViewModel
@@ -94,8 +105,16 @@ namespace WebStore.Controllers
                 Profession = Model.Profession,
                 Department = Model.Department,
             };
-            if (__EmployeesData.Edit(employee))
+            if (Model.Id == 0)
+            {
+                _EmployeesData.Add(employee);
+                _Logger.LogInformation("Создан новый сотрудник {0}", employee);
+            }
+            else if (!_EmployeesData.Edit(employee))
+            {
+                _Logger.LogInformation("Информация о сотруднике {0} изменена", employee);
                 return NotFound();
+            }
 
             return RedirectToAction("Index");
         }
