679adbf8760b45c8d13a62ad2255897c51620b63
Yuliya <yvnovikova@mail.ru>
2021-12-23 22:45:54 +0300

    Интерфейс сервиса каталога товаров

--- WebStore.Domain/Entities/Brand.cs
+++ WebStore.Domain/Entities/Brand.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WebStore.Domain.Entities.Base;
+using WebStore.Domain.Entities.Base.Interface;
+
 
 namespace WebStore.Domain.Entities
 {
-    public class Brand
+    public class Brand : NamedEntity, IOrderedEntity
     {
-
+        public int Order { get; set; }
     }
+    
 }
---
+++ WebStore/Services/Interfaces/IProductData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebStore.Domain.Entities;
+
+namespace WebStore.Services.Interfaces
+{
+    public interface IProductData
+    {
+        IEnumerable<Section> GetSections();
+        IEnumerable<Section> GetBrans();
+    }
+}
