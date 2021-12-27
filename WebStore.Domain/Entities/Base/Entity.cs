cbf14b92ec3c916ae3588d9fbb99256c9f968501
Yuliya <yvnovikova@mail.ru>
2021-12-27 22:22:59 +0300

    Подготовка модели данных для размещения ее в БД

--- WebStore.Domain/Entities/Base/Entity.cs
+++ WebStore.Domain/Entities/Base/Entity.cs
@@ -3,12 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using WebStore.Domain.Entities.Base.Interface;
 
 namespace WebStore.Domain.Entities.Base
 {
     public abstract class Entity: IEntity
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; } 
     }
 }
--- WebStore.Domain/Entities/Section.cs
+++ WebStore.Domain/Entities/Section.cs
@@ -4,15 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using WebStore.Domain.Entities.Base;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using WebStore.Domain.Entities.Base.Interface;
+using Microsoft.EntityFrameworkCore;
 
 namespace WebStore.Domain.Entities
 {
+    [Index(nameof(Name),IsUnique = true)]
     public class Section : NamedEntity, IOrderedEntity
     {
         public int Order { get; set; }
 
+        //public int? ParentId { get; set; } = null -- При наличии "?" у типа данных можно указать что поле может иметь нулевое значение
         public int? ParentId { get; set; }
+
+        [ForeignKey(nameof(ParentId))]//--Внешний ключ
+        public Section Parent { get; set; } //-- Навигационное свойство
+
+        public ICollection<Product> Products { get; set; }
     }
 
 }
