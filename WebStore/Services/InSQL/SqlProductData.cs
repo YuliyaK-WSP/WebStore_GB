61f5b050f4afc8a36c3905a9b8e024c75727d20d
Yuliya <yvnovikova@mail.ru>
2022-01-13 19:31:44 +0300

    Контроллер корзины

--- WebStore/Services/InSQL/SqlProductData.cs
+++ WebStore/Services/InSQL/SqlProductData.cs
@@ -1,47 +1,43 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using WebStore.DAL.Context;
 using WebStore.Domain;
 using WebStore.Domain.Entities;
 using WebStore.Services.Interfaces;
 
-namespace WebStore.Services.InSQL
+namespace WebStore.Services.InSQL;
+
+public class SqlProductData : IProductData
 {
-    public class SqlProductData : IProductData
+    private readonly WebStoreDB _db;
+
+    public SqlProductData(WebStoreDB db) => _db = db;
+
+    public IEnumerable<Section> GetSections() => _db.Sections;
+
+    public IEnumerable<Brand> GetBrands() => _db.Brands;
+
+    public IEnumerable<Product> GetProducts(ProductFilter? Filter = null)
     {
-        private readonly WebStoreDB _db;
-        public SqlProductData(WebStoreDB db) => _db = db;
-        public IEnumerable<Brand> GetBrands() => _db.Brands;
+        IQueryable<Product> query = _db.Products
+           .Include(p => p.Brand)
+           .Include(p => p.Section);
 
-        public Product? GetProductById(int Id) => _db.Products
-            .Include(p=>p.Brand)
-            .Include(p=> p.Section)
-            .FirstOrDefault(p=>p.Id == Id);
-
-        public IEnumerable<Product> GetProducts(ProductFilter? Filter = null)
+        if (Filter?.Ids?.Length > 0)
+            query = query.Where(product => Filter.Ids.Contains(product.Id));
+        else
         {
-            IQueryable<Product> query = _db.Products;
+            if (Filter?.SectionId is { } section_id)
+                query = query.Where(p => p.SectionId == section_id);
 
-            if (Filter?.Ids!.Length > 0)
-            {
-                query = query.Where(product => Filter.Ids.Contains(product.Id));
-            }
-            else
-            {
-                if (Filter?.SectionId is { } section_id)
-                    query = query.Where(p => p.SectionId == section_id);
-                if (Filter?.BrandId is { } brand_id)
-                    query = query.Where(p => p.BrandId == brand_id);
-            }
-            return query;
+            if (Filter?.BrandId is { } brand_id)
+                query = query.Where(p => p.BrandId == brand_id);
         }
 
-        public IEnumerable<Section> GetSections() => _db.Sections;
-
-        
+        return query;
     }
+
+    public Product? GetProductById(int Id) => _db.Products
+       .Include(p => p.Brand)
+       .Include(p => p.Section)
+       .FirstOrDefault(p => p.Id == Id);
 }
